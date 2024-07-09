e675f36c17e19bae2b72192fe8b3d986bd788ef8
bhautik <bhautikchothani52@gmail.com>
2024-07-09 13:14:52 +0530

    portfolio

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,19 @@
 export const Bio = {
-  name: "Shivam Shah",
+  name: "Bhautik Chothani",
   roles: [
     "Full Stack Developer",
-    "Java Web Developer",
-    "System Designer",
+    "python Web Developer",
     "DataBase Manager",
   ],
   description:
-    "A full-stack Java/web developer currently employed as a Backend Developer at Smartters.in. I have expertise in the MERN/MEAN stack and also mentor college graduates in Node.js and Feathers.js. Before this role, I worked as a Teaching Assistant at Dhirubhai Ambani Institute of Information and Communication Technology, where I guided students in Object-Oriented Programming and problem-solving. I possess comprehensive knowledge of Java, JavaScript, Spring, RESTful Web Services, React.js, and Feathers.js.",
-  github: "https://github.com/shi-vam7902",
+    "I am a dynamic full-stack web developer based in Ahmedabad, Gujarat. Currently, I mentor college graduates, empowering them with skills in MySql, Flask, FastAPI, and python.I possess expertise in python technologie like RESTful Web Services and FASTAPI",
+  github: "https://github.com/bhautikchothani",
   resume:
-    "https://drive.google.com/file/d/1KjyHO9DsMI05igtVhn6nwhlXqQ8f0JkT/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/shah-shivam/",
+    "https://drive.google.com/file/d/1DrOpbBJQ_TKhA7lEXcofWLdi4uUQbqBm/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/bhautik-chothani-1175b720b/",
   twitter: "",
-  insta: "https://www.instagram.com/pluviophil0709?igsh=ejlid3U1ZTZiYnBm",
-  facebook: "https://www.facebook.com/shah.shivam.1000",
+  insta: "https://www.instagram.com/bhautik_patel6/",
+  facebook: "https://www.facebook.com/bhautik.chothani.52",
 };
 
 export const skills = [
@@ -31,29 +30,9 @@ export const skills = [
           "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
       },
       {
-        name: "Three Js",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/threejs/threejs-original.svg",
-      },
-      {
         name: "JavaScript",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-      },
-      {
-        name: "Tailwind Css",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-      },
-      {
-        name: "Bootstrap",
-        image:
-          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
-      },
-      {
-        name: "Material UI",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg",
       },
     ],
   },
@@ -61,34 +40,9 @@ export const skills = [
     title: "Programming Languages",
     skills: [
       {
-        name: "C",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
-      },
-      {
-        name: "C++",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
-      },
-
-      {
         name: "Python",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-      },
-      {
-        name: "Java 8,11",
-        image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
-      },
-      {
-        name: "Php ",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
-      },
-      {
-        name: "Node Js",
-        image: "https://nodejs.org/static/images/logo.svg",
       },
     ],
   },
@@ -96,19 +50,9 @@ export const skills = [
     title: "DataBases",
     skills: [
       {
-        name: "Microsoft Server Management Studio 18",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/microsoftsqlserver/microsoftsqlserver-original.svg",
-      },
-      {
         name: "MySql 9.5",
         image:
           "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-      },
-      {
-        name: "MongoDb",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg",
       },
       {
         name: "PostgreSql",
@@ -120,66 +64,19 @@ export const skills = [
     title: "Tools And FrameWorks",
     skills: [
       {
-        name: "Servlet Jsp",
-        image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
-      },
-      {
-        name: "Angular 8^",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angularjs/angularjs-original.svg",
-      },
-      {
-        name: "Spring Web",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg",
-      },
-      {
-        name: "Spring BOOT ",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original.svg",
-      },
-      {
-        name: "React Js",
-        image:
-          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
-      },
-      {
         name: "GitHub",
         image:
           "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
       },
-
       {
         name: "Postman",
         image:
           "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
       },
       {
-        name: "Express Js",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg",
-      },
-      {
-        name: "Eclipse",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eclipse/eclipse-original.svg",
-      },
-
-      {
         name: "Vercel",
         image:
           "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original-wordmark.svg",
-      },
-      {
-        name: "Maven",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/maven/maven-original.svg",
-      },
-      {
-        name: "Apache Tomcat Server",
-        image:
-          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apache/apache-original.svg",
       },
     ],
   },
@@ -191,110 +88,58 @@ export const experiences = [
     img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/download.png",
     role: "Full Stack Web Intern [TL]",
     company: "Xcelcode Skill Up Serve",
-    date: "May 2024 - Present",
+    date: "January 2024 - Present",
     desc: "",
     skills: [
-      "Node Js",
-      "React Js",
-      "AWS",
-      "EC2",
       "Python",
       "Django",
-      "JavaScript",
-    ],
+      "Flask",
+      "FastApi",
+      "MySql",
+      "Html5",
+      "CSS3",
+      ],
     doc: "",
   },
-  {
-    id: 0,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/iitp.png",
-    role: "Research Intern",
-    company: "IIT - Patna",
-    date: "June 2023 - Present",
-    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
-    skills: [
-      "ReactJS",
-      "Redux",
-      "NodeJs",
-      "Material UI",
-      "HTML",
-      "CSS",
-      "JavaScript",
-    ],
-    doc: "",
-  },
-  {
-    id: 2,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/unnati.jpeg",
-    role: "Jr. Full Stack Web Developer (MEAN Stack)",
-    company: "Unnati Informatics LLP",
-    date: "June 2022 - April 2023",
-    desc: "Worked with back-end developers to design APIs Developed functional databases, applications, and servers to support websites on back-end.Collaborated with team members and gave efficient input.",
-    doc: "",
-    skills: ["Node Js", "Angular", "Firebase-Deployment", "Material UI"],
-  },
-  {
-    id: 3,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/nit.png",
-    role: "Cyber Security Research Intern",
-    company: "NIT Trichy",
-    date: "April 2022 - July 2022 ",
-    desc: "As an Cyber Security Intern  at the National Institute Of Trichy (NIT), .",
-    skills: [
-      "Power Grid Management",
-      "Devloping Secure Communications ",
-      "Smart Grid Security ",
-      "Research",
-      "Tiruchirappalli",
-    ],
-  },
-  {
-    id: 4,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/royal.jpeg",
-    role: "Boot Camp Intern",
-    company: "Royal Technosoft Private limited",
-    date: "July 2020 - Nov 2020",
-    desc: "As an Intern at Royal Technosoft Private limited, Ahmedabad, Gujarat , I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
-    skills: ["C", "C++", "HTML", "JS"],
-    doc: "",
-  },
+  
 ];
 
 export const education = [
   {
     id: 0,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/daiict.png",
-    school: "Dhirubhai Ambani Institute Of ICT Gandhinagar",
-    date: "June 2023 - Current",
-    grade: "CPI-7",
-    desc: "Currently pursuing M.Sc. in Information Technology at DA-IICT, specializing in Full Stack Web Development and Cloud Computing. Skilled in MERN stack, Firebase, and REST-API development. Experienced in project management and deployment on Vercel and Heroku. Passionate about enhancing digital solutions in healthcare and e-commerce sectors.",
-    degree: "MSC'IT",
+    img: "https://silveroakuni.ac.in/upload/images/250x70.webp",
+    school: "Silver Oak University",
+    date: "June 2022 - Current",
+    grade: "CPI-8.22",
+    desc: "Currently pursuing M.C.A in Information Technology at Silver Oak University. Skilled in FullStack and REST-API development. Experienced in project management and deployment on Vercel. Passionate about enhancing digital solutions in healthcare and e-commerce sectors.",
+    degree: "MCA",
   },
   {
     id: 1,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/gls.jpeg",
-    school: "Gls University ",
-    date: "July 2020 - April 2023",
-    grade: "8.67 CGPA",
-    desc: "  I have successfully completed my Bachelor's degree in Computer Applications From GLS University, CG Road Ahmedabad, with a CGPA of 8.67.I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. .",
+    img: "https://i1.rgstatic.net/ii/institution.image/AS%3A267467195125770%401440780556357_l",
+    school: "Msu University ",
+    date: "Aug 2019 - Sep 2021",
+    grade: "5.7 CGPA",
+    desc: "  I have successfully completed my Bachelor's degree in Computer Applications From MSU University, BARODA, with a CGPA of 5.7. I have taken courses in B.COM.",
     degree:
-      "Bachelor of Computer Applications - BCA, Computer Science and Engineering",
+      "Bachelor of Commerce - B.COM",
   },
   {
     id: 2,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/dbs.jpeg",
-    school: "Divine Buds English School",
-    date: "June 2019 - March 2020",
-    grade: "63.20%",
-    desc: "I completed my class 12 high school education at Divine Buds High School, Himmatnagar.",
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_l1_0UtahJ3rwuUv9TlkJWjCOxOl-HToXTiUXT8g_7-r3A=s900-c-k-c0x00ffffff-no-rj",
+    school: "Shree G.D Paneliya Higher Secondary School",
+    date: "June 2018 - May 2019",
+    grade: "66.04%",
+    desc: "I completed my class 12 high school education at Shree G.D Paneliya Higher Secondary School, Ankolwadi(Gir).",
     degree: "GSEB(XII)",
   },
   {
     id: 3,
-    img: "https://raw.githubusercontent.com/shi-vam7902/imaages/main/stblz.png",
-    school: "ST Blaze English School",
+    img: "",
+    school: "Pulkit School",
     date: "June 2017 - April 2018",
-    grade: "69.83%",
-    desc: "I completed my class 10 education at ST.Blaze English School, Ahmedabad.",
+    grade: "71.58%",
+    desc: "I completed my class 10 education at Pulkit School, Junagadh.",
     degree: "GSEB(X)",
   },
 ];
@@ -302,112 +147,16 @@ export const education = [
 export const projects = [
   {
     id: 9,
-    title: "D-Pharma",
-    date: "Jun 2022 - Jul 2022",
+    title: "ChatBot",
+    date: "Jun 2024 - April 2024",
     description:
-      "D Pharmaceuticals is an established pharmaceutical based at Gujarat. We are into Franchisee marketing of (Finished Dosage Formulations) & have own Marketing, Sales, Distribution team at Gujarat. We Ethically promote the scientific products to various specialities such as., Dermatologists, Gynecologists, Pediatrics, General Medicine, Surgeons, Orthopaedicians, Cardiologists , Diabetologists etc., We strongly believe in Quality & ensure that all the products are FDA certified.",
+      "A chatbot project created in Python, utilizing the LLaMA 3 model, leverages a large language model to engage users in natural, human-like conversations. The FastAPI framework and Tortoise ORM manage the backend, providing a robust, scalable solution for real-time interaction and data handling. This setup ensures efficient, responsive, and intelligent chatbot performance.",
     image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/dpharma.png",
-    tags: ["MongoDb", "Node Js", "Express Js", "Angular"],
+      "https://private-user-images.githubusercontent.com/92966944/346330062-44523ef6-28c2-4770-9aa8-c477d3fe1d37.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjAzNTA4MzMsIm5iZiI6MTcyMDM1MDUzMywicGF0aCI6Ii85Mjk2Njk0NC8zNDYzMzAwNjItNDQ1MjNlZjYtMjhjMi00NzcwLTlhYTgtYzQ3N2QzZmUxZDM3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzA3VDExMDg1M1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWY0MDVmODg2YjZiZjQ0ZGNkOTRhMDhhMmY1ZTIyZWVkNTM3NzU4YWMxNjM1M2EwZGU1Y2Q0ZTNjY2E2MGU5ZGImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.pln8Shnwr6ntZsNYp1RAhEfIo9x9FgDEc3hgGbG_-Ak",
+    tags: ["Python", "Django", "Docker", "Postgresql"],
     category: "web app",
     github: "",
-    webapp: "https://dpharma-cfb92.web.app/Home",
-  },
-  {
-    id: 1,
-    title: "HappyPlaces.com",
-    date: "Oct 2022 - April 2023",
-    description:
-      "Designed and developed the The Happy Places Project, a project that tracks the exact pin-point location",
-    image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/happyplaces.com.png",
-    tags: [
-      "Angular",
-      "MongoDb",
-      "Node Js",
-      "Express Js",
-      "NodeMailer",
-      "Mapjs",
-      "location.js",
-    ],
-    category: "web app",
-    github: "",
-    webapp: "",
-  },
-
-  {
-    id: 10,
-    title: "Collobrative Task Management project",
-    date: "May 2024 - Jun 2024",
-    description:
-      "Collobratative Task Management System specifically developed for the companies wanting to keep a track of projects tasks and employees.",
-    image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/cmts.png",
-    tags: [
-      "React Native",
-      "JavaScript",
-      "Axios",
-      "vercel",
-      "NodeJs",
-      "Postman",
-    ],
-    category: "node js",
-    github: "https://github.com/shi-vam7902/CTMS-backend",
-    webapp: "https://github.com/shi-vam7902/CTMS_frontend",
-  },
-  {
-    id: 10,
-    title: "AstroChat.com",
-    date: "March 2024 - July 2024",
-    description:
-      "Comprehensive Chatting Applications for Astrologers and users.",
-    image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/astrochat.png",
-    tags: [
-      ,
-      "JavaScript",
-      "Axios",
-      "vercel",
-      "NodeJs",
-      "Postman",
-      "vite",
-      "Node js",
-    ],
-    category: "node js",
-    github: "https://github.com/StefSalv07/frontend-astrochat",
-    webapp: "https://github.com/StefSalv07/astroChat-backend",
-  },
-  {
-    id: 10,
-    title: "Outfits On Rent",
-    date: "March 2023 - May 2023",
-    description: "Exclusive Rental Service for Traditional ",
-    image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/outfits.png",
-    tags: [
-      "React Native",
-      "JavaScript",
-      "Axios",
-      "render",
-      "NodeJs",
-      "Postman",
-      "React Js",
-    ],
-    category: "node js",
-    github: "https://github.com/shi-vam7902/outfit_backend",
-    webapp: "",
-  },
-  {
-    id: 4,
-    title: "E-Holiday",
-    date: "Jul 2021",
-    description: "Travelling Website In Advance java .",
-    image:
-      "https://raw.githubusercontent.com/shi-vam7902/imaages/main/1.eholiday_deployment_page.png",
-    tags: ["React Js", "Firebase", "Firestore", "Node JS"],
-    category: "java",
-    github: "",
-    webapp: "",
+    webapp: "", // Deployment link   
   },
 ];
 
